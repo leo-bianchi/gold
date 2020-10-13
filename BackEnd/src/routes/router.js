b3c841eff3201d82d7626760d2aa2911dfb81427
Leo Bianchi <leo.c.bianchi@hotmail.com>
2020-10-13 19:56:00 -0300

    updates

--- BackEnd/src/routes/router.js
+++ BackEnd/src/routes/router.js
@@ -1,26 +1,25 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-router.get('/myProfile', (req, res, next) => {
-    const myProfile = require('../data/myprofile.json');
-    res.status(200).send({
-        myProfile
-    });
+router.get("/myProfile", (req, res, next) => {
+  const myProfile = require("../data/myprofile.json");
+  res.status(200).send({
+    myProfile,
+  });
 });
 
-
-router.get('/', (req, res, next) => {
-    const index = require('../data/index.json');
-    res.status(200).send({
-        index
-    });
+router.get("/", (req, res, next) => {
+  const index = require("../data/index.json");
+  res.status(200).send({
+    index,
+  });
 });
 
-router.get('/friendProfile', (req, res, next) => {
-    const friendProfile = require('../data/friendProfile.json');
-    res.status(200).send({
-        friendProfile
-    });
+router.get("/friendProfile", (req, res, next) => {
+  const friendProfile = require("../data/friendProfile.json");
+  res.status(200).send({
+    friendProfile,
+  });
 });
 
 module.exports = router;
--- js/script.js
+++ js/script.js
@@ -1,33 +1,10 @@
 M.AutoInit();
-document.addEventListener("DOMContentLoaded", () => {
-  const contact = document.querySelector(".btn__contact") || "";
-  const contact2 = document.querySelector(".btn__contact2") || "";
-  const elem = document.querySelector(".carousel");
 
-  (function callCarousel() {
-    const options = {
-      fullWidth: true,
-      indicators: true,
-    };
-    M.Carousel.init(elem, options);
-  })();
-
-  if (contact) {
-    (function callInfoAlert() {
-      contact.onclick = () => {
-        Swal.fire(
-          "Ótimo!",
-          "Sua solicitação foi enviada para um moderador.",
-          "info"
-        );
-      };
-    })();
-  }
-  if (contact2) {
-    (function callInfoAlert() {
-      contact2.onclick = () => {
-        Swal.fire("Salvo!", "", "info");
-      };
-    })();
-  }
-});
+const elem = document.querySelector(".carousel");
+(function callCarousel() {
+  const options = {
+    fullWidth: true,
+    indicators: true,
+  };
+  M.Carousel.init(elem, options);
+})();
